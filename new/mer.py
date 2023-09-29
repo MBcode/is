4f646d5dd2aa8ddac0c2066035b5e72cb8099a0c
mike bobak <MBcode@users.noreply.github.com>
2023-09-29 14:40:38 -0500

    rm unneeded

--- new/mer.py
+++ new/mer.py
@@ -16,29 +16,9 @@ def os_system_(cs):
 
 import pyperclip as p
 
-def get_ent_cb():
-    ne = p.waitForPaste()
-    cs = f'./get_entities.sh "{ne}" csLex'
-    print(cs)
-    r = os_system_(cs)
-    print(r)
-    return r
-
-def t1():
-    return get_ent_cb()
-
-def get_ent_cnb():
-    ne = p.waitForNewPaste(45)
-    cs = f'./get_entities.sh "{ne}" csLex'
-    print(cs)
-    r = os_system_(cs)
-    print(r)
-    return r
-
-#def t2():
-#    return get_ent_cnb()
 
 def get_ent(n=None): #could send in lex
+    "run MER to get the concept tagging of the text"
     lex=os.getenv("lex")
     if lex==None:
         lex="csLex"
@@ -53,12 +33,14 @@ def get_ent(n=None): #could send in lex
     return r
 
 def ccwc():
+    "wc on cp buffer"
     #cs = f'pbpaste |wc'
     cs = f'xclip -selection clipboard -o |wc'
     n = os_system_(cs)
     return n
 
 def ccc(fn):
+    "file to cp buffer"
     #cs = f'cat {fn} |pbcopy'
     cs = f'cat {fn} |xclip -selection clipboard'
     #cs = f'cat {fn} |xclip -i'
@@ -77,18 +59,13 @@ def maccp(s):
     cs = f'echo {s} |xclip -selection clipboard'
 
 def ccsay():
+    "read the cp buffer aloud"
     #cs = 'pbpaste | espeak --stdin'
     cs = f'xclip -selection clipboard -o | espeak --stdin -s 245 -p 70'
     os_system(cs)
 
-#ccmo | cut -d'/' -f5 | sortucn
-#to get count of concepts
-#_might have alt links w/scoring to sum that as well
-#alias sortucn 'sort | uniq -c |sort -n'
-#could have the ret put in the cp buffer, then run the count on that
-#if using ccsay would have2run that before overwritting the buffer
 def cccounts(s):
-    #don't need cp buffer
+    "listing of concept counts"
     cs = f"echo '{s}'|cut -d'/' -f5 |sort | uniq -c |sort -rn"
     #print(cs)
     r = os_system_(cs)
@@ -97,6 +74,7 @@ def cccounts(s):
 
 
 def get_ent_fn(fn):
+    "entities tagged from a file"
     ccc(fn)
     return get_ent()
 
@@ -118,4 +96,3 @@ if __name__ == '__main__':
     cccounts(r)
     ccsay()
     #print(r)
-    #cccounts(r)
